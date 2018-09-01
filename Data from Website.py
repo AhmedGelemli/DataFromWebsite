cfe97b2aa93eb34c9645f8fc801bdc2e9e2ed96a
kalemli <kalemli@gmail.com>
2018-09-01 21:26:20 +0400

    deleted some codes

--- Data from Website.py
+++ Data from Website.py
@@ -11,32 +11,8 @@ print()
 print('ok')
 print('test messages..')
 
-option = input("Select for option >>>")
-url_input_first = input("Is your site 1)HTTP or 2)HTTPS >>>")
-url_input = input("Please enter your website (ex:domain.com) >>>")
-
-
 def http():
   myurl = rq.get("http://" + url_input)
-  if (option == "1"):
-    myurl.content
-    soup = BeautifulSoup(myurl.content)
-    print(soup.prettify())
-  elif (option == "2"):
-    tag_name = input("Please Enter Tag (ex: h1, p, a):")
-    myurl.content
-    soup = BeautifulSoup(myurl.content)
-    linkler = soup.find_all(tag_name)
-    for link in linkler:
-      print(link)
-  elif (option == "3"):
-    tag_name_forelement = input("Please Enter Tag Name:")
-    element = input("Please Enter Element:")
-    myurl.content
-    linkler = soup.find_all(tag_name_forelement)
-    for link in linkler:
-      soup = BeautifulSoup(myurl.content)
-      print(link.get(element))
 
 def https():
   myurl = rq.get("https://" + url_input)
